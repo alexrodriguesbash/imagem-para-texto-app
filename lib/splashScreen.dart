636b167452ad83db56ade1b4ce8ea94593f9eaef
alexrodriguesbash <alek.rodrigues@hotmail.com>
2021-07-24 17:33:17 -0300

    Adicionada as páginas splashScreen.dart e homePage.dart

---
+++ lib/splashScreen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:imagem_para_texto_app/homePage.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+
+class MySplashScreen extends StatefulWidget {
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 15,
+      navigateAfterSeconds: HomePage(),
+      title: Text(
+        'Converta imagem em texto',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          color: Colors.black,
+        ),
+      ),
+      image: Image.asset("assets/imagetext.png"),
+      photoSize: 130,
+      backgroundColor: Colors.white,
+      loaderColor: Colors.blue,
+      loadingText: Text(
+        "Aguarde ...",
+        style: TextStyle(
+          color: Colors.blue,
+          fontSize: 16.0,
+        ),
+      ),
+    );
+  }
+}
